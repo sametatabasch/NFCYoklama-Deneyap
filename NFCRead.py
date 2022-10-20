3f6f8d1f4be8ea0b9335c2267068dca8cd1f7ac0
sametatabasch <sametatabasch@gmail.com>
2022-10-20 20:59:29 +0300

    KArt okunduğunda buzzer ile bip sesi çıkartma özelliği eklendi

--- NFCRead.py
+++ NFCRead.py
@@ -1,17 +1,27 @@
 # NFCRead.py -- put your code here!
 from lib import mfrc522
-from machine import Pin, SPI
+from machine import Pin, SPI, PWM
 from utime import sleep
 import deneyap
 from config import uidlist
 
+'''
+Set Pins
+'''
+Pmiso = Pin(deneyap.MISO)
+Pmosi = Pin(deneyap.MOSI)
+Psck = Pin(deneyap.SCK)
+Pbuzzer = Pin(deneyap.D15, Pin.OUT)
+
 
 def do_read():
-    spi = SPI(1, baudrate=2500000, polarity=0, phase=0, miso=Pin(deneyap.MISO), mosi=Pin(5), sck=Pin(19))
+    spi = SPI(1, baudrate=2500000, polarity=0, phase=0, miso=Pmiso, mosi=Pmosi, sck=Psck)
     spi.init()
-    rdr = mfrc522.MFRC522(spi=spi, gpioRst=23, gpioCs=4)
+    rdr = mfrc522.MFRC522(spi=spi, gpioRst=deneyap.D0, gpioCs=deneyap.SDA)
 
     waiting_to_read = False
+    buzzer = PWM(Pbuzzer)
+    buzzer.freq(1047)
 
     while True:
         if not waiting_to_read:
@@ -26,8 +36,9 @@ def do_read():
 
             if stat == rdr.OK:
                 waiting_to_read = False
+                buzzer.duty(80)
                 uid = "0x%02x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3], raw_uid[4])
-                print( uid,"idli kart okundu")
+                print(uid, "idli kart okundu")
                 '''
                 if rdr.select_tag(raw_uid) == rdr.OK:
                     print(uidlist[uid], "Okundu")
@@ -35,3 +46,6 @@ def do_read():
                     print("Select failed")
                 '''
         sleep(0.5)
+        buzzer.duty(0)
+
+    buzzer.deinit()
