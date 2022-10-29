fb49f4d920e4448201245af8cfbeecd000bedecb
sametatabasch <sametatabasch@gmail.com>
2022-10-29 22:22:42 +0300

    Fazla dosyalar silindi. Ana sınıfa eklendi

--- NFCAttendance.py
+++ NFCAttendance.py
@@ -5,6 +5,13 @@ from lib.mfrc522 import MFRC522
 from lib.pcd8544 import PCD8544_FRAMEBUF
 from machine import Pin, SPI
 import deneyap
+import network
+import config
+from time import sleep
+
+wlan = None
+
+
 
 
 class NFCAttendance():
@@ -18,12 +25,13 @@ class NFCAttendance():
         nfc_spi = SPI(1, baudrate=2500000, polarity=0, phase=0, miso=pin_miso, mosi=pin_mosi, sck=pin_sck)
         lcd_spi = SPI(2, baudrate=2000000, polarity=0, phase=0, miso=pin_miso, mosi=pin_mosi, sck=pin_sck)
         nfc_spi.init()
-        lcd_spi.init()
-        self.NFC = MFRC522(spi=nfc_spi, gpioRst=deneyap.D0, gpioCs=deneyap.SDA)
+        # lcd_spi.init()
+        self.NFC = MFRC522(spi=nfc_spi, gpioRst=deneyap.D14, gpioCs=deneyap.SDA)
         lcd_cs = Pin(deneyap.D13)
         lcd_dc = Pin(deneyap.D12)
         lcd_rst = Pin(deneyap.D1)
         self.LCD = PCD8544_FRAMEBUF(spi=lcd_spi, cs=lcd_cs, dc=lcd_dc, rst=lcd_rst)
+        self.connect_wifi()
 
     def write_lesson_name(self):
         lesson_name = "BILP-100"
@@ -51,3 +59,38 @@ class NFCAttendance():
         """
         self.LCD.text(msg, self.center(msg), (n - 1) * 8, 1)
         self.LCD.show()
+
+    def read_student_card(self):
+        waiting_to_read = False
+        while True:
+            if not waiting_to_read:
+                print("Kart Okutun")
+                waiting_to_read = True
+
+            (stat, tag_type) = self.NFC.request(self.NFC.REQIDL)
+
+            if stat == self.NFC.OK:
+                (stat, raw_uid) = self.NFC.anticoll()
+
+                if stat == self.NFC.OK:
+                    waiting_to_read = False
+                    uid = "0x%02x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3], raw_uid[4])
+                    print(uid, "idli kart okundu")
+                    '''
+                    if rdr.select_tag(raw_uid) == rdr.OK:
+                        print(uidlist[uid], "Okundu")
+                    else:
+                        print("Select failed")
+                    '''
+
+    @staticmethod
+    def connect_wifi():
+        global wlan
+        wlan = network.WLAN(network.STA_IF)  # create a wlan object
+        wlan.active(True)  # Activate the network interface
+        wlan.disconnect()  # Disconnect the last connected WiFi
+        wlan.connect(config.wifi['ssid'], config.wifi['password'])  # connect wifi
+        while (wlan.ifconfig()[0] == '0.0.0.0'):
+            sleep(1)
+            pass
+        return True
--- boot.py
+++ boot.py
@@ -1,40 +1,13 @@
 # boot.py -- run on boot-up
-import NFCRead
-import NFCWrite
-import config
-import network
-import time
 from NFCAttendance import NFCAttendance
 
-
-wlan = None
-
-
-def connect_wifi():
-    global wlan
-    wlan = network.WLAN(network.STA_IF)  # create a wlan object
-    wlan.active(True)  # Activate the network interface
-    wlan.disconnect()  # Disconnect the last connected WiFi
-    wlan.connect(config.wifi['ssid'], config.wifi['password'])  # connect wifi
-    while (wlan.ifconfig()[0] == '0.0.0.0'):
-        time.sleep(1)
-        pass
-    return True
-
-
-connect_wifi()
 try:
     app = NFCAttendance()
-    while True:
-        islem = input("İşlem seçin: 1- Okuma, 2- Yazma, 0- çıkış\n")
-        if (islem == '1'):
-            NFCRead.do_read()
-        elif (islem == '2'):
-            app.LCD.clear()
-            app.write_lesson_name()
-            app.write_waiting_message()
-        else:
-            break
+    app.LCD.clear()
+    app.write_lesson_name()
+    app.write_waiting_message()
+
+    app.read_student_card()
 
 except KeyboardInterrupt:
     print("İşlem Sonlandırıldı ")
