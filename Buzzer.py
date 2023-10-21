1a048ad50bf9798ad895c58ee0b097350e5036c8
sametatabasch <sametatabasch@gmail.com>
2023-10-22 02:34:47 +0300

    pin out ve pin pull down eklendi duty değer değiştirildi

--- Buzzer.py
+++ Buzzer.py
@@ -1,13 +1,14 @@
 from machine import Pin, PWM, DAC
 import time
 
+
 class Buzzer:
     def __init__(self, pin_number):
-        self.buzzer_pin = Pin(pin_number)
+        self.buzzer_pin = Pin(pin_number, Pin.OUT, Pin.PULL_DOWN)
         self.buzzer_pwm = PWM(self.buzzer_pin)
 
     def beep(self, frequency, duration_ms):
         self.buzzer_pwm.freq(frequency)  # Frekansı ayarla
-        self.buzzer_pwm.duty(1000)  # Şiddeti ayarla (0-1023 arası)
+        self.buzzer_pwm.duty(512)  # Şiddeti ayarla (0-1023 arası)
         time.sleep_ms(duration_ms)
         self.buzzer_pwm.duty(0)
