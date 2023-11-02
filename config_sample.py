764fb8bfe1cf081c1509c40e3045280f75415a26
sametatabasch <sametatabasch@gmail.com>
2023-11-03 01:08:24 +0300

    config dosyasının örneği oluşturuldu

---
+++ config_sample.py
@@ -0,0 +1,29 @@
+import deneyap
+
+wifi = [
+    {
+        'ssid': '',
+        'password': ""
+    },
+    {
+        'ssid': '',
+        'password': ""
+    }
+]
+
+api_username = ""
+api_passwd = ""
+api_url = ""
+
+Pins = {
+    "buzzer_pin": deneyap.A4,
+
+    "keypad_row1": deneyap.D12,
+    "keypad_row2": deneyap.D13,
+    "keypad_row3": deneyap.D14,
+    "keypad_row4": deneyap.D15,
+    "keypad_col1": deneyap.DAC2,
+    "keypad_col2": deneyap.DAC1,
+    "keypad_col3": deneyap.A5,
+    "keypad_col4": deneyap.D4,
+}
